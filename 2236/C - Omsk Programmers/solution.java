154f269883869201be0a9260d68b42bc4bf28be5
Abhishek Gupta <abhishekgupta29072005@gmail.com>
2026-07-27 21:59:15 +0530

    Add Omsk Programmers [C] from Codeforces

---
+++ 2236/C - Omsk Programmers/solution.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
+        int t = sc.nextInt();
+        long[] u = new long[35];
+        long[] v = new long[35];
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long x = sc.nextLong();
+            int c1 = 0;
+            long curr = a;
+            u[c1++] = curr;
+            while (curr > 0) {
+                curr /= x;
+                u[c1++] = curr;
+            }
+            int c2 = 0;
+            curr = b;
+            v[c2++] = curr;
+            while (curr > 0) {
+                curr /= x;
+                v[c2++] = curr;
+            }
+            long ans = Math.abs(a - b);
+            for (int i = 0; i < c1; i++) {
+                for (int j = 0; j < c2; j++) {
+                    long cost = (long) i + j + Math.abs(u[i] - v[j]);
+                    if (cost < ans) {
+                        ans = cost;
+                    }
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
